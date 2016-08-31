9f82bf029d72ce36a96abd519aaa9d3bfe664799
Andy Theuninck <andy@gohanman.com>
2016-08-30 19:17:58 -0500

    Use proper tokens instead of regex to check for namespaces

--- src/ClassCache.php
+++ src/ClassCache.php
@@ -6,6 +6,10 @@ use \RuntimeException;
 
 class ClassCache
 {
+    const BRACE_NS = 1;
+    const SEMICOLON_NS = 2;
+    const UNKNOWN_NS = -1;
+
     /**
       Get definitions for several classes and write them out to
       a single file
@@ -36,12 +40,18 @@ class ClassCache
     {
         $def = str_replace('<?php', '', $def);
         $def = str_replace('?>', '', $def);
-        if (!preg_match('/\s*namespace\s+/', $def)) {
+        $tokens = token_get_all('<?php ' . $def);
+        $numNS = $this->countNamespaces($tokens);
+        if ($numNS > 1) {
+            // class isn't cacheable
+            return '';
+        }
+        if ($numNS == 0) {
             $def = "\nnamespace {\n"
                 . $def
                 . "\n}\n";
-        } elseif (preg_match('/\s*namespace\s+(.*);/', $def, $matches)) {
-            $def = preg_replace('/\s*namespace\s+.*;/', "namespace {$matches[1]} {", $def);
+        } elseif ($numNS == 1 && $this->typeOfNamespace($tokens) == self::SEMICOLON_NS) {
+            $def = preg_replace('/\s*namespace\s+(.*)\s*;/', "namespace $1 {", $def);
             $def .= "\n}\n";
         }
 
@@ -60,6 +70,38 @@ class ClassCache
         }
 
         return $files;
+    }
+
+    private function countNamespaces($tokens)
+    {
+        $count = 0;
+        foreach ($tokens as $t) {
+            if (is_array($t) && $t[0] == T_NAMESPACE) {
+                $count++;
+            }
+        }
+
+        return $count;
+    }
+
+    private function typeOfNamespace($tokens)
+    {
+        for ($i=0; $i<count($tokens); $i++) {
+            if (is_array($tokens[$i]) && $tokens[$i][0] == T_NAMESPACE) {
+                $j = $i+1;
+                while (is_array($tokens[$j]) && ($tokens[$j][0] == T_WHITESPACE || $tokens[$j][0] == T_STRING)) {
+                    $j++;
+                }
+                if ($tokens[$j] == ';') {
+                    return self::SEMICOLON_NS;
+                } elseif ($tokens[$j] == '{') {
+                    return self::BRACE_NS;
+                }
+                break;
+            }
+        }
+
+        return self::UNKNOWN_NS;
     }
 
     /**
